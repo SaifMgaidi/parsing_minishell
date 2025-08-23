99dde77c3d70f0b89211a2fe29b338937b6ba42a
saif <saif4547@gmail.com>
2025-08-23 09:37:08 +0200

    update lexer.c

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -13,6 +13,59 @@ int	count_words(char **words)
 	return (count);
 }
 
+
+t_token_type	get_token_type(char *word)
+{
+	if (!ft_strncmp(word, ">>", 2))
+		return (APPEND);
+	else if (!ft_strncmp(word, "<<", 2))
+		return (HEREDOC);
+	else if (!(ft_strncmp(word, ">", 1)))
+		return (REDIR_OUT);
+	else if (!ft_strncmp(word, "<", 1))
+		return (REDIR_IN);
+	else if (!ft_strncmp(word, "]", 1))
+		return (PIPE);
+	else
+		return (WORD);
+
+}
+
+
+int	create_token(t_token **tokens, char *word)
+{
+	t_token	*tok;
+
+	if (!tokens)
+		return (0);
+	if (!(*tokens))
+	{
+		tok = malloc(sizeof(t_token));
+		if (!tok)
+			return (0);
+		(*tokens) = tok;
+	}
+	else
+	{
+		tok = (*tokens);
+		while (tok->next)
+			tok = tok->next;
+		tok->next = malloc(sizeof(t_token));
+		if (!(tok->next))
+			return (0);
+		tok = tok->next;
+	}
+	tok->value = ft_strdup(word);
+	if (!tok->value)
+	{
+		free(tok);
+		return (0);
+	}
+	tok->type = get_token_type(tok->value);
+	tok->next = NULL;
+	return (1);
+}
+
 t_token	*ft_tokenize(char *line)
 {
 	char	**words;
@@ -25,18 +78,15 @@ t_token	*ft_tokenize(char *line)
 	words = split_line(line);
 	if (!words)
 		return (NULL);
-	len_words = count_words(words);
-	tokens = malloc(sizeof(t_token) * len_words);
-	if (!tokens)
-	{
-		free(words);
-		return (NULL);
-	}
+	tokens = NULL;
 	i = 0;
 	while (words[i])
 	{
-		
+		if (!create_token(&tokens, words[i]));
+			break ;
+		i++;
 	}
+	return (tokens);
 }
 
 int	main(int argc, char *argv[])
